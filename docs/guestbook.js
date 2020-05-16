8c3eca3dc8b3f747de92525e14d934b15a26b9a0
Rudiger Wolf <rudiger.wolf@throughputfocus.com>
2020-05-16 13:46:47 +0100

    Update with new API

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://CHANGEME.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://b5ba918c.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
